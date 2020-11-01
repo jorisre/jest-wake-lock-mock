1178fb83ce5e383f4c4b7d738323724898027e99
jorisre <reixjoris@gmail.com>
2020-11-01 23:40:50 +0100

    test: add wakeLock handles request then release multiple times test case

--- test/jest-wake-lock-mock.test.ts
+++ test/jest-wake-lock-mock.test.ts
@@ -25,6 +25,7 @@ test('wakeLock handles request then release with success', async () => {
     expect(wakeLock?.type).toEqual('screen');
     expect(wakeLock?.released).toBe(false);
 
+    // Handle wakeLock release
     wakeLock?.release();
     expect(wakeLock?.type).toEqual('screen');
     expect(wakeLock?.released).toBe(true);
@@ -46,4 +47,58 @@ test('wakeLock handles request and throw an error', async () => {
   expect(wakeLock).not.toBeDefined();
   expect(error).toMatchInlineSnapshot(`[Error: wakeLock error]`);
   expect(handleRelease).not.toHaveBeenCalled();
+});
+
+test('wakeLock handles request then release multiple times', async () => {
+  // First time
+  const firstHandleRelease = jest.fn();
+  const { wakeLock, error } = await requestWakeLock(firstHandleRelease);
+
+  expect(error).not.toBeDefined();
+  expect(wakeLock).toBeDefined();
+  expect(firstHandleRelease).not.toHaveBeenCalled();
+
+  if (wakeLock) {
+    const onReleaseSpy = jest.spyOn(wakeLock, 'onrelease');
+
+    expect(wakeLock?.type).toEqual('screen');
+    expect(wakeLock?.released).toBe(false);
+
+    // Handle wakeLock release
+    wakeLock?.release();
+    expect(wakeLock?.type).toEqual('screen');
+    expect(wakeLock?.released).toBe(true);
+    expect(firstHandleRelease).toHaveBeenCalledWith(expect.any(Event));
+    expect(onReleaseSpy).toHaveBeenCalledWith(expect.any(Event));
+
+    wakeLock?.removeEventListener('release', firstHandleRelease);
+    expect(firstHandleRelease).toHaveBeenLastCalledWith(expect.any(Event));
+  }
+
+  // Nd time
+  const ndHandleRelease = jest.fn();
+  const { wakeLock: ndWakeLock, error: ndError } = await requestWakeLock(
+    ndHandleRelease
+  );
+
+  expect(ndError).not.toBeDefined();
+  expect(ndWakeLock).toBeDefined();
+  expect(ndHandleRelease).not.toHaveBeenCalled();
+
+  if (ndWakeLock) {
+    const onReleaseSpy = jest.spyOn(ndWakeLock, 'onrelease');
+
+    expect(ndWakeLock?.type).toEqual('screen');
+    expect(ndWakeLock?.released).toBe(false);
+
+    // Handle wakeLock release
+    ndWakeLock?.release();
+    expect(ndWakeLock?.type).toEqual('screen');
+    expect(ndWakeLock?.released).toBe(true);
+    expect(ndHandleRelease).toHaveBeenCalledWith(expect.any(Event));
+    expect(onReleaseSpy).toHaveBeenCalledWith(expect.any(Event));
+
+    ndWakeLock?.removeEventListener('release', ndHandleRelease);
+    expect(ndHandleRelease).toHaveBeenLastCalledWith(expect.any(Event));
+  }
 });
