6bdaf031ceb3a65ce6bfdfd8c2c8c9376f0e5a7b
jorisre <reixjoris@gmail.com>
2020-11-08 15:29:40 +0100

    ci: use default semantic-release config

--- release.config.js
+++ release.config.js
@@ -1,14 +1,3 @@
 module.exports = {
   branches: ['main'],
-  plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        preset: 'conventionalcommits',
-      },
-    ],
-    '@semantic-release/npm',
-    '@semantic-release/git',
-    '@semantic-release/github',
-  ],
 };
